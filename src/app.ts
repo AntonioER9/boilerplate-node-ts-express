bd930b3356c2711ce671b2d45a8dda5b315fff8e
AntonioER9 <aespinozareyes9@gmail.com>
2024-07-04 11:07:01 -0400

    first commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import { Server } from "./server";
+
+(() => {
+  main();
+})();
+
+function main() {
+  const server = new Server();
+  server.start();
+}
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+export class Server {
+  
+  private app = express();
+
+  async start() {
+    console.log('server running');
+
+    this.app.listen(3000, () => {
+      console.log(`Server running on port ${3000}`);
+    });
+
+  }
+
+}
